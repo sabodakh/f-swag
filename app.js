699011a1366498a550d6508d75c7ed5e258ca295
Yurii <georgesfranko@gmail.com>
2021-02-01 16:42:20 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const navBar = document.querySelector('.navbar-nav');
+navBar.addEventListener('mouseover', openMenu);
+
+// Open dropdown menu
+function openMenu(e){
+   console.log(e.target.parentElement);
+    
+}
